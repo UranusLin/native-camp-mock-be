2ed99f634e505d906e50866f2890a82b967f632f
MorrisLin <aka611511@gmail.com>
2021-09-18 22:41:34 +0800

    Feat: Initial commit

---
+++ src/main/java/com/mock/nativecamp/NativeCampApplication.java
@@ -0,0 +1,13 @@
+package com.mock.nativecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeCampApplication.class, args);
+    }
+
+}
